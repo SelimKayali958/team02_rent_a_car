eaed43ce606aa52be4bfab194927d409ed8d286a
SelimKayali958 <selimkayali958@gmail.com>
2020-08-28 17:36:50 +0300

    liste hazirlik

--- src/team02_ilk_java_proje/GirisMenusu.java
+++ src/team02_ilk_java_proje/GirisMenusu.java
@@ -1,7 +1,13 @@
 package team02_ilk_java_proje;
 
+import java.util.Scanner;
+
 public class GirisMenusu {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Team02 Rent A Car uyglamasinina hos geldiniz. Lutfen asagidaki islemlerden birini seciniz" +
+                "\n1-) Araclarimiz\n2-) Arac Kirala\n3-) Hakkimizda\n4-) Iletisim\n5-) Kampanyalar\n6-) Cikis Yap");
 
     }
 }
