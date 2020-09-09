63fb225750b78499b7d9b2ea5088019911da5b8e
SelimKayali958 <selimkayali958@gmail.com>
2020-09-09 23:38:09 +0300

    09.09.20 tarihli guncelleme

---
+++ src/team02_ilk_java_proje/DizelArabalar.java
@@ -0,0 +1,93 @@
+package team02_ilk_java_proje;
+
+public class DizelArabalar {
+    public static void VolkswagenJetta(){
+        String marka = "Volkswagen";
+        String model = "Jetta";
+        int yil = 2016;
+        int km = 258000;
+        String vites = "Manuel";
+        int gunlukKira = 250 ;
+
+        System.out.println("Marka :"+marka+ "\nModel : "+model+"\nYil : "+yil+"\nKm : "+km+"\nVites tipi : "+vites
+                +"\nGunluk kira Bedeli : "+gunlukKira);
+    }
+    public static void MercedesBenz(){
+        String marka = "Mercedes";
+        String model = "Benz";
+        int yil = 1990 ;
+        int km = 321000;
+        String vites = "Otomatik";
+        int gunlukKira = 200;
+
+        System.out.println("Marka :"+marka+ "\nModel : "+model+"\nYil : "+yil+"\nKm : "+km+"\nVites tipi : "+vites
+                +"\nGunluk kira Bedeli : "+gunlukKira);
+    }
+    public static void FordFocus(){
+        String marka = "Ford";
+        String model = "Focus";
+        int yil =2011 ;
+        int km =315000 ;
+        String vites = "Manuel";
+        int gunlukKira = 220;
+
+        System.out.println("Marka :"+marka+ "\nModel : "+model+"\nYil : "+yil+"\nKm : "+km+"\nVites tipi : "+vites
+                +"\nGunluk kira Bedeli : "+gunlukKira);
+    }
+    public static void BMW4Serisi(){
+        String marka = "BMW";
+        String model = "4 serisi";
+        int yil = 2016;
+        int km = 87000;
+        String vites = "Otomatik";
+        int gunlukKira = 260;
+
+        System.out.println("Marka :"+marka+ "\nModel : "+model+"\nYil : "+yil+"\nKm : "+km+"\nVites tipi : "+vites
+                +"\nGunluk kira Bedeli : "+gunlukKira);
+    }
+    public static void AudiA6(){
+        String marka = "Audi";
+        String model = "A6";
+        int yil =2016;
+        int km = 81000;
+        String vites = "Yarı Otomatik";
+        int gunlukKira = 230;
+
+        System.out.println("Marka :"+marka+ "\nModel : "+model+"\nYil : "+yil+"\nKm : "+km+"\nVites tipi : "+vites
+                +"\nGunluk kira Bedeli : "+gunlukKira);
+    }
+    public static void RenaultFluence(){
+        String marka = "Renault";
+        String model = "Fluence";
+        int yil =2015;
+        int km = 110000;
+        String vites = "Manuel";
+        int gunlukKira = 200;
+
+        System.out.println("Marka : Renault \nModel : Fluence \nYil : 2015 \nKm : 110.000 \nVites tipi : Manuel"
+                +"\nGunluk kira Bedeli : 200tl");
+    }
+    public static void SkodaOctavia(){
+        String marka = "Scoda";
+        String model = "Octavia";
+        int yil = 2019;
+        int km = 26000;
+        String vites = "Yarı Otomatik";
+        int gunlukKira = 270;
+
+        System.out.println("Marka :"+marka+ "\nModel : "+model+"\nYil : "+yil+"\nKm : "+km+"\nVites tipi : "+vites
+                +"\nGunluk kira Bedeli : "+gunlukKira);
+    }
+    public static void Peugeot301(){
+        String marka = "Peugeot";
+        String model = "301";
+        int yil = 2018;
+        int km = 12000;
+        String vites = "Manuel";
+        int gunlukKira = 280;
+
+        System.out.println("Marka :"+marka+ "\nModel : "+model+"\nYil : "+yil+"\nKm : "+km+"\nVites tipi : "+vites
+                +"\nGunluk kira Bedeli : "+gunlukKira);
+    }
+
+}
